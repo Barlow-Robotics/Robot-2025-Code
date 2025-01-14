532086622907a1e06c1e5189b42c3155cb123253
doctorsus <turtle195@outlook.com>
2025-01-13 20:14:39 -0500

    base logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,21 +4,50 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.ElectronicsIDs;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+    Logger.recordMetadata("ProjectName", "2025-Robot-Code"); // Set a metadata value
+
+    if (isReal()) {
+        Logger.addDataReceiver(new WPILOGWriter("/media/sda2/")); // Log to a USB stick
+        Logger.addDataReceiver(new NT4Publisher());
+        // Publish data to NetworkTables
+        // CHANGE - leaks below
+        new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
+    } else {
+        Logger.addDataReceiver(new WPILOGWriter(""));
+        Logger.addDataReceiver(new NT4Publisher());
+    }
+
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may
+                    // be added.
   }
 
   @Override
   public void robotPeriodic() {
+    String currentDriverController = DriverStation.getJoystickName(ElectronicsIDs.DriverControllerPort);
+    String currentOperatorController = DriverStation.getJoystickName(ElectronicsIDs.OperatorControllerPort);
+    Logger.recordOutput("Controllers/Driver", currentDriverController);
+    Logger.recordOutput("Controllers/Operator", currentOperatorController);
+
     CommandScheduler.getInstance().run();
   }
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
