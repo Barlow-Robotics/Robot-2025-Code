f8fba7b650c0cc4a4cecad25eacc826117911391
BrianWeiss1 <brianweissct@gmail.com>
2025-03-17 19:25:37 -0400

    Fixed L3 Path

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -394,7 +394,7 @@ public class Drive extends TunerSwerveDrivetrain implements Subsystem {
 
             // return Commands.sequence(getFullCommand(finalPath), new WaitCommand(2), getFullCommand(finalPath_2));
         // 
-            return Commands.sequence(getFullCommand(finalPath), new WaitCommand(3), getFullCommand(finalPath_2), new WaitCommand(1), getFullCommand(finalPath_3));
+            return Commands.sequence(getFullCommandWithReset(finalPath), new WaitCommand(3), getFullCommand(finalPath_2), new WaitCommand(1), getFullCommand(finalPath_3));
 
             // return Commands.sequence(new WaitCommand(1), getFullCommand(finalPath), new WaitCommand(1), /*sequentalCommand1.asProxy(), sequentalCommand1.asProxy(),*/ new WaitCommand(3), getFullCommand(finalPath_2), new WaitCommand(1), getFullCommand(finalPath_3));
 
@@ -407,9 +407,13 @@ public class Drive extends TunerSwerveDrivetrain implements Subsystem {
     }
 
 
-    private Command getFullCommand(PathPlannerPath finalPath) {
+    private Command getFullCommandWithReset(PathPlannerPath finalPath) {
         return AutoBuilder.followPath(finalPath).alongWith(Commands.runOnce(() -> resetPose(finalPath.getStartingHolonomicPose().get())));
     }
+    private Command getFullCommand(PathPlannerPath finalPath) {
+        return AutoBuilder.followPath(finalPath);
+    }
+
 
     
     public Command ChoreoAutoWithoutReset(String name) {
